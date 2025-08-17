7812737403ba1ec20ade97d4bf9cda0633756494
deepat1128 <deepaoum1128@gmail.com>
2025-08-17 22:43:18 +0530

    Update variable.tf

--- windows-vm/variable.tf
+++ windows-vm/variable.tf
@@ -1,34 +1,34 @@
 variable rgname {
-	default = "rg-deepa"
+	default = "rg-deepa-win"
 	}
 variable location {
 	default = "West US 2"
 	}
 variable vnetname {
-	default = "vnet-deepa"
+	default = "vnet-deepa-win"
 	}
 variable addresspace {
-	default = ["10.0.0.0/16"]
+	default = ["10.2.0.0/16"]
 	type = list(string)
 	}
 
 variable snetname{
-			default = "snet-deepa"
+			default = "snet-deepa-win"
 		}
 	variable saddresspace{
-			default = ["10.0.1.0/24"]
+			default = ["10.2.1.0/24"]
 			type = list(string)
 		}
 variable pip{
-			default = "publicip-deepa"
+			default = "publicip-deepa-win"
 		}
 
 variable nsg{
-			default = "nsg-deepa"
+			default = "nsg-deepa-win"
 		}
 variable nic{
-			default = "nic-deepa"
+			default = "nic-deepa-win"
 		}
 		variable ipconfigname{
-			default = "ipdeepa"
+			default = "ipdeepa-win"
 		}
