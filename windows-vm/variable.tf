a612855642bdb00446f15a692d5525174b921dba
deepat1128 <deepaoum1128@gmail.com>
2025-08-17 23:47:03 +0530

    Update variable.tf

--- windows-vm/variable.tf
+++ windows-vm/variable.tf
@@ -68,3 +68,7 @@ variable "image" {
     version   = "latest"
   }
 }
+output "vm_public_ip" {
+  description = "Public IP address of the VM"
+  value       = azurerm_public_ip.example.ip_address
+}
