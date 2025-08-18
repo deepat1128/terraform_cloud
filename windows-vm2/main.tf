b7387a4d3086509969adbc17f2ea788e95d390c5
deepat1128 <deepaoum1128@gmail.com>
2025-08-19 00:07:43 +0530

    Update main.tf

--- windows-vm2/main.tf
+++ windows-vm2/main.tf
@@ -1,31 +1,46 @@
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "remote" {
+    organization = "your-org-name"
+
+    workspaces {
+      name = "windows-vm"
+    }
+  }
+}
 
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "rg-windows-vm"
+  location = "East US"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.vm_name}-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
+  name                = "vnet-windows"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "${var.vm_name}-subnet"
+  name                 = "subnet-windows"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name}-nic"
-  location            = var.location
+  name                = "nic-windows"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
@@ -36,13 +51,15 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_windows_virtual_machine" "vm" {
-  name                = var.vm_name
+  name                = "winvm01"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  size                = "Standard_DS1_v2"
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B2s"
   admin_username      = var.admin_username
   admin_password      = var.admin_password
-  network_interface_ids = [azurerm_network_interface.nic.id]
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
 
   os_disk {
     caching              = "ReadWrite"
@@ -55,7 +72,4 @@ resource "azurerm_windows_virtual_machine" "vm" {
     sku       = "2022-Datacenter"
     version   = "latest"
   }
-
-  enable_automatic_updates = true
-  provision_vm_agent       = true
 }
