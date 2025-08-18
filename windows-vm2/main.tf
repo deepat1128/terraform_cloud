9c02a2e155e4990c173bf4d90cd45aa6c6caec30
deepat1128 <deepaoum1128@gmail.com>
2025-08-18 23:58:45 +0530

    Update main.tf

--- windows-vm2/main.tf
+++ windows-vm2/main.tf
@@ -1,31 +1,35 @@
 
 
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-${var.vm_name}"
+  name                = "${var.vm_name}-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-${var.vm_name}"
+  name                 = "${var.vm_name}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "nic-${var.vm_name}"
+  name                = "${var.vm_name}-nic"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "ipconfig"
+    name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
@@ -39,14 +43,19 @@ resource "azurerm_windows_virtual_machine" "vm" {
   admin_username      = var.admin_username
   admin_password      = var.admin_password
   network_interface_ids = [azurerm_network_interface.nic.id]
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
     sku       = "2022-Datacenter"
     version   = "latest"
   }
+
+  enable_automatic_updates = true
+  provision_vm_agent       = true
 }
