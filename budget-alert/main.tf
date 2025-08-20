91e6c8b57a18977efd02435a4699d74ad4d58dd2
deepat1128 <deepaoum1128@gmail.com>
2025-08-20 13:12:10 +0530

    Update main.tf

--- budget-alert/main.tf
+++ budget-alert/main.tf
@@ -1,15 +1,15 @@
 
 
 
-data "azurerm_resource_group" "vmss_rg" {
+data "azurerm_resource_group" "target" {
   name = var.resource_group_name
 }
 
 resource "azurerm_consumption_budget_resource_group" "vmss_budget" {
-  name                = "vmss-budget"
-  resource_group_name = var.resource_group_name
-  amount              = 500.0
-  time_grain          = "Monthly"
+  name              = "vmss-budget"
+  resource_group_id = data.azurerm_resource_group.target.id
+  amount            = 500.0
+  time_grain        = "Monthly"
 
   time_period {
     start_date = "2025-08-01T00:00:00Z"
@@ -17,17 +17,17 @@ resource "azurerm_consumption_budget_resource_group" "vmss_budget" {
   }
 
   filter {
-    dimensions {
-      name = "ResourceType"
+    dimension {
+      name   = "ResourceType"
       values = ["Microsoft.Compute/virtualMachineScaleSets"]
     }
   }
 
-  notifications {
-    enabled        = true
-    operator       = "GreaterThan"
-    threshold      = 90.0
-    contact_emails = ["deepaoum1128@gmail.com"]
+  notification {
+    enabled         = true
+    operator        = "GreaterThan"
+    threshold       = 90.0
+    contact_emails  = ["deepaoum1128@gmail.com"]
   }
 }
 
